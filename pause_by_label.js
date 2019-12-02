04a59a7155710cb098fc1fa2c799559778003ab5
camiltang <camiltang@gmail.com>
2019-12-03 02:00:16 +0800

    Create pause_by_label.js
    Initial commit

---
+++ pause_by_label.js
@@ -0,0 +1,56 @@
+/**
+ *  
+ * @description: Google Ads - Pause campaign by script at budget limit. 
+ * @author: Camil Tang-Turcotte
+ * 
+ */
+
+// ----- Settings --------------------------------------- 
+
+var budget = 1000; // enter budget here (is spend on twitter)
+var client = ''; // client name here 
+var safe_buffer = 0.025; // %
+var email = ''; // email to send alert here
+var io_startdate = "20190101"; // YYYYMMDD
+var io_enddate = "20191231"; // YYYYMMDD
+var label = 'Brand'; // Select Campaigns by Label
+
+// ----- Function --------------------------------------- 
+
+function main() {
+  var spent = getcost();
+  var currentAccount = AdsApp.currentAccount();
+  Logger.log('Customer ID: ' +  currentAccount.getCustomerId() + '\nAccount: ' + currentAccount.getName() + '\nCurrency Code: ' + currentAccount.getCurrencyCode() + '\nSpend: ' + spent.toFixed(2) + ' $ \nBudget: ' + budget.toFixed(2) + ' $ \nAttached campaignns were paused. \n');
+
+  if (spent > (budget * (1 - safe_buffer))) {
+    var campaigns = AdWordsApp.campaigns().withCondition("Status = ENABLED").withCondition("LabelNames CONTAINS_ANY [" + label + "]").get();
+    var csv = 'Campaign ID,Campaign';
+    
+    while (campaigns.hasNext()) { // iterate around all campaigns with Label
+      var campaign = campaigns.next();
+      campaign.pause();
+      var row = [campaign.getId(),campaign.getName()];
+      csv += '\n' + row.join(',');
+    }
+    
+    MailApp.sendEmail(email, 'Campaign paused by Script | Budget limit | ' + client, Logger.getLog(),{attachments:[{fileName: 'paused_campaigns.csv', mimeType: 'text/csv', content: csv}]});
+    Logger.log('SENT')
+  }
+  else {
+    Logger.log("Not Sent")
+  }
+}
+
+function getcost() {
+  var value = 0;
+  var campaigns = AdsApp.campaigns().withCondition("LabelNames CONTAINS_ANY [" + label + "]").get();
+
+  while (campaigns.hasNext()) { // iterate around all campaigns with Label
+    var campaign = campaigns.next();
+    var cost = campaign.getStatsFor(io_startdate, io_enddate).getCost();
+    value = value + cost;
+  }
+  
+  //Logger.log(value.toFixed(2) + ' | Total'); 
+  return value; //return cost value for entire period, of campaign with Label. 
+}
